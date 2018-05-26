6900b728131063d05143f6e19ea56ee029a51e1d
andresdigio <adigiovannimartinez@itba.edu.ar>
2018-05-26 20:12:28 -0300

    received first get method :)

--- src/server/host.c
+++ src/server/host.c
@@ -12,39 +12,69 @@
 #include "message.h"
 
 #define BUFF_SIZE 1024
-#define PORT_NUMBER 9090
+#define PORT_NUMBER 8085
+
+typedef enum method {CONNECT, GET, POST, HEAD} method_t;
+
+typedef struct {
+    char * hostname;
+    method_t method;
+} request_t;
 
 struct sockaddr_in* findIp(char* hostname);
 void connectToOrigin(struct sockaddr_in* host, char* content, int length);
 
-int connectToClient(){
+int connectToServer(struct sockaddr_in* host){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (sockfd < 0){
+        DieWithSystemMessage("socket() failed");
+    }
+
+    host->sin_port = htons(80);
+
+    if(connect(sockfd, (struct sockaddr*)host, sizeof(*host)) < 0){
+        DieWithSystemMessage("connect() failed");
+    }
+
+    return sockfd;
 }
 
 int main(int argc, char const *argv[]) {
-    int sockfd, newsockfd, n;
+
+    int mSock, cliSock, servSock, n;
     socklen_t clilen;
     char * inBuffer = malloc(BUFF_SIZE);
-    struct sockaddr_in serv_addr, cli_addr;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT_NUMBER);
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    fflush(stdout);
 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    struct sockaddr_in *my_addr = malloc(sizeof(struct sockaddr_in));
+    struct sockaddr_in *cli_addr = malloc(sizeof(struct sockaddr_in));
+    struct sockaddr_in *serv_addr = malloc(sizeof(struct sockaddr_in));
+
+    mSock = socket(AF_INET, SOCK_STREAM,0);
+    bzero((char *) my_addr, sizeof(struct sockaddr_in));
+    serv_addr->sin_family = AF_INET;
+    serv_addr->sin_port = htons(PORT_NUMBER);
+    serv_addr->sin_addr.s_addr = INADDR_ANY;
+
+    int enable = 1;
+    if (setsockopt(mSock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+        DieWithSystemMessage("setsockopt(SO_REUSEADDR) failed");
+
+
+    if (bind(mSock, serv_addr, sizeof(struct sockaddr_in)) < 0)
         DieWithUserMessage("ded","ERROR on binding");
-    listen(sockfd,5);
+    listen(mSock,5);
 
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    clilen = sizeof(struct sockaddr_in);
+    cliSock = accept(mSock, cli_addr, &clilen);
 
-    if (newsockfd < 0)
+    if (cliSock < 0)
       DieWithSystemMessage("ERROR on accept");
 
     bzero(inBuffer,1024);
-    n = recv(newsockfd,inBuffer,BUFF_SIZE,0);
+    n = recv(cliSock,inBuffer,BUFF_SIZE,0);
 
     if (n < 0)
         DieWithSystemMessage("ERROR reading from socket");
@@ -53,9 +83,31 @@ int main(int argc, char const *argv[]) {
 
     char* hostname = parseRequest(inBuffer, n);
 
-    connectToOrigin(findIp(hostname), inBuffer, n);
+    serv_addr = findIp(hostname);
+
+    if((servSock = connectToServer(serv_addr)) >= 0){
+        char connectedMessage[128];
+        char * okString = "HTTP/1.1 200 Connection established\r\n\r\n";
+        strcpy(connectedMessage, okString);
+        send(cliSock, okString, strlen(okString),0);
+    }
+
+    bzero(inBuffer,1024);
+    char aux[2048];
+    n = recv(cliSock,aux,BUFF_SIZE,0);
+
+    if (n < 0)
+        DieWithSystemMessage("ERROR reading from socket");
+
+    printf(aux);
+    //connectToOrigin(findIp(hostname), inBuffer, n);
+
+
 
     free(hostname);
+    close(mSock);
+    close(cliSock);
+    close(servSock);
 
     return 0;
 }
